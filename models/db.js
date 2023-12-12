03a2733374b57d19e5734b17d9786a36ffdab060
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2023-12-12 11:29:10 -0400

    fix: :ambulance: Fixed data fetch using Sequelize, MySQL and NodeJS

--- models/db.js
+++ models/db.js
@@ -1,9 +1,8 @@
 const Sequelize = require('sequelize');
 
-// Database Connection
 const database = new Sequelize('sistemadeestoque', 'root', '2315469', {
     host: "localhost",
     dialect: 'mysql'
 });
 
-module.exports = { Sequelize, database }
+module.exports = { Sequelize: Sequelize, database: database }
